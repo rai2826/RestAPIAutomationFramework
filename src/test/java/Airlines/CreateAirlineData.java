edb9c7cb4b912a02d9d952c1842383566ddf6349
Gaurav Rai <gaurav.rai@team.telstra.com>
2023-06-19 15:50:42 +1000

    Added create airline test

---
+++ src/test/java/Airlines/CreateAirlineData.java
@@ -0,0 +1,34 @@
+package Airlines;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateAirlineData {
+
+    @Test
+    public void insertairlinedata(){
+
+       Response response= RestAssured.given()
+                .baseUri("https://api.instantwebtools.net/v1/airlines")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"id\": 153179,\n" +
+                        "    \"name\": \"GRV Airways\",\n" +
+                        "    \"country\": \"GRV\",\n" +
+                        "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
+                        "    \"slogan\": \"From Rewathipur\",\n" +
+                        "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
+                        "    \"website\": \"www.srilankaairways.com\",\n" +
+                        "    \"established\": \"1987\"\n" +
+                        "}")
+               .log().all()
+                .post();
+
+
+        Assert.assertEquals(response.statusCode(),200);
+
+    }
+}
